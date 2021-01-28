43c8d1a281abae2376bef3f9cc8ab6209900cb06
Loïc CHAUDY <chaudy@et.esiea.fr>
2021-01-28 10:46:05 +0100

    Improve matrix class.

--- src/Utils/Matrix.h
+++ src/Utils/Matrix.h
@@ -5,66 +5,318 @@
 #include <iostream>
 #include <stdexcept>
 
+/**
+ * @class Matrix
+ * @brief Class that manage matrix calculations.
+ *
+ * Class that manage matrix calculations.
+ * Addition, Subtraction, product with scalar, product with matrix and more.
+ *
+ */
 class Matrix
 {
 public:
-    /* Construct */
+    /**
+     * @brief Basic constructor with line and column.
+     *
+     * This is a basic constructor for a matrix. All the values are set to 0.
+     *
+     * @param nbLine
+     *     Number of line for the matrix.
+     *
+     * @param nbCol
+     *      Number of column for the matrix.
+     */
     Matrix(std::size_t nbLine, std::size_t nbCol);
+
+    /**
+     * @brief Another constructor with line and column and value init.
+     *
+     * This is another constructor for a matrix. We also use a initializer_list to init the values of the matrix's element.
+     *
+     * @param nbLine
+     *     Number of line for the matrix.
+     *
+     * @param nbLine
+     *      Number of column for the matrix.
+     *
+     * @param initializerList
+     *      List to init the values os the matrix.
+     */
     Matrix(std::size_t nbLine,
            std::size_t nbCol,
            const std::initializer_list<std::initializer_list<double>>& initializerList);
+
+    /**
+     * @brief Constructor by copy.
+     *
+     */
     Matrix(const Matrix& /*A*/);
 
-    /* Destruct */
+    /**
+     * @brief Destructor of our class.
+     *
+     * Free the memory.
+     *
+     */
     ~Matrix();
 
-    /* Define value of the matrix */
+    /**
+     * @brief Set the values of the matrix manually.
+     *
+     * Update the values of the matrix with a terminal.
+     *
+     */
     void setvalue();
 
-    /* Print */
+    /**
+     * @brief Print a matrix.
+     *
+     *
+     */
     void printMatrix();
 
-    /* Addition */
+    /**
+     * @brief Addition of two matrix.
+     *
+     * The matrix must have the same dimensions.
+     *
+     * @param a
+     *     One matrix.
+     *
+     * @param b
+     *      A second matrix.
+     *
+     * @return
+     *      Return the result of the addition of matrix a and b.
+     */
     static Matrix addMat(Matrix& a, Matrix& b);
 
-    /* Substraction */
+    /**
+     * @brief Subtraction of two matrix.
+     *
+     * The matrix must have the same dimensions.
+     *
+     * @param a
+     *     One matrix.
+     *
+     * @param b
+     *      A second matrix.
+     *
+     * @return
+     *      Return the result of the subtraction of matrix a and b (a-b).
+     */
     static Matrix subMat(Matrix& a, Matrix& b);
 
-    /* Scalar mutliplication */
+    /**
+      * @brief Multiplication with a scalar.
+      *
+      * Multiply all the values with a scalar.
+      *
+      * @param a
+      *     One matrix.
+      *
+      * @param scalar
+      *      A scalar the we want to multiply the matrix with.
+      *
+      * @return
+      *      Return the result of the multiplication of the matrix and the scalar.
+     */
     static Matrix scalMult(Matrix& a, double scalar);
 
-    /* matrix product */
+    /**
+      * @brief Multiplication two matrix.
+      *
+      * Multiply two matrix. Verify that they have the necessary dimension to permit this action.
+      *
+      *
+      * @param a
+      *     One matrix.
+      *
+      * @param b
+      *      Second matrix.
+      *
+      * @return
+      *      Return the result of the multiplication of the two matrix.
+      *      Be safe of the order : a*b.
+     */
     static Matrix prodMat(Matrix& a, Matrix& b);
 
-    /* translation  (Vec3) || dependance de AddMat */
+    /**
+      * @brief Translation of one 3D vector.
+      *
+      * Translate a 3D vector with another 3D vector.
+      * Be careful, this only works with 3D vector.
+      *
+      * @see addMat
+      *
+      * @param a
+      *     One 3D vector.
+      *
+      * @param b
+      *      3D vector which represent the direction.
+      *
+      * @return
+      *      Return a nex 3D vector resulting of the translation of the vector a by the direction b.
+      *      The operation is the following : a+b.
+     */
     static Matrix translation(Matrix& a, Matrix& b);
 
-    /* Rotation (Vec3)  || dep avec prodMat */
-    /*   /!\ To modif    */
+    /**
+      * @brief Rotation on x axis.
+      *
+      * Rotate a 3D vector on the x axis.
+      * Be careful, this only works with 3D vector.
+      * The rotation matrix is 3*3 matrix.
+      *
+      * @see prodMat
+      *
+      * @param alpha
+      *     The angle of the rotation.
+      *
+      * @param a
+      *      3D vector to rotate. Must be a 3D vector
+      *
+      * @return
+      *      Return a new 3D vector resulting of the rotation of the vector on the x axis.
+     */
     static Matrix rotationX(double alpha, Matrix& a);
+
+    /**
+      * @brief Rotation on y axis.
+      *
+      * Rotate a 3D vector on the y axis.
+      * Be careful, this only works with 3D vector.
+      * The rotation matrix is 3*3 matrix.
+      * There is a dependency with the prodMat method.
+      *
+      *
+      * @param alpha
+      *     The angle of the rotation.
+      *
+      * @param a
+      *      3D vector to rotate. Must be a 3D vector
+      *
+      * @return
+      *      Return a new 3D vector resulting of the rotation of the vector on the y axis.
+     */
     static Matrix rotationY(double alpha, Matrix& a);
+
+    /**
+      * @brief Rotation on z axis.
+      *
+      * Rotate a 3D vector on the z axis.
+      * Be careful, this only works with 3D vector.
+      * The rotation matrix is 3*3 matrix.
+      * There is a dependency with the prodMat method.
+      *
+      *
+      * @param alpha
+      *     The angle of the rotation.
+      *
+      * @param a
+      *      3D vector to rotate. Must be a 3D vector
+      *
+      * @return
+      *      Return a new 3D vector resulting of the rotation of the vector on the z axis.
+      */
     static Matrix rotationZ(double alpha, Matrix& a);
 
-    /* Scale  || dep avec prodMat */
+    /**
+      * @brief Scale a 3D vector.
+      *
+      * Be careful, this only works with 3D vector.
+      * The scaling matrix is 3*3 matrix.
+      *
+      * @see prodMat
+      *
+      * @param x
+      *     The coefficient for the x component of the 3D vector.
+      *
+      * @param y
+      *      The coefficient for the y component of the 3D vector.
+      *
+      * @param z
+      *      The coefficient for the z component of the 3D vector.
+      *
+      *  @param a
+      *      The 3D vector.
+      *
+      * @return
+      *      Return a new 3D vector resulting of the scaling depending of the coefficient.
+     */
     static Matrix scale(double x, double y, double z, Matrix& a);
 
-    /* Transposition of a matrix*/
+    /**
+      * @brief Transposition of a matrix.
+      *
+      *
+      * @param a
+      *     The matrix that we want to transpose.
+      *
+      * @return
+      *      Return a new matrix which is the transpose of the matrix in parameter.
+     */
     static Matrix transposed(Matrix& a);
 
-    /* Get the norm of a Vec3 (Vec3)*/
+    /**
+      * @brief Get the norm of a 3D vector.
+      *
+      * Be careful, this only works with 3D vector.
+      *
+      * @param a
+      *     The 3D vector.
+      *
+      * @return
+      *      Return the norm of the 3D vector in parameter.
+     */
     static double getNorm(Matrix& a);
 
-    /* Nomalize a Vec3 || dep with getNorm*/
+    /**
+      * @brief Normalize a 3D vector.
+      *
+      * Be careful, this only works with 3D vector.
+      *
+      * @see getNorm
+      *
+      * @param a
+      *     The 3D vector.
+      *
+      * @return
+      *      Return a new 3D vector which is the normalized vector of a.
+     */
     static Matrix normalize(Matrix& a);
 
+    /**
+      * @brief Get the value of one item in the matrix.
+      *
+      * @param line
+      *     The line's location of our element.
+      *
+      * @param column
+      *     The column's location of our element.
+      *
+      * @return
+      *      Return the value of the element at the given line and column.
+     */
     double getValue(std::size_t line, std::size_t column);
 
 
 private:
+    /**
+     * The number of the line of our matrix.
+     */
     std::size_t m_dimL = 0;
+
+    /**
+     * The number of the column of our matrix.
+     */
     std::size_t m_dimC = 0;
 
+    /**
+     * Store the element of our matrix.
+     */
     double** m_matrix = nullptr;
 };
 
-#endif //H_LIBTRACE_MATRIX_H
+#endif //H_RAYTRACING_MATRIX_H
--- tests/src/Utils/Matrix.cpp
+++ tests/src/Utils/Matrix.cpp
@@ -1,8 +1,21 @@
 #include <Utils/Matrix.h>
 #include <doctest.h>
 
+bool absoluteToleranceCompare(double x, double y)
+{
+    return std::fabs(x - y) <= std::numeric_limits<double>::epsilon();
+}
+
+bool approximatelyEqual(double a, double b, double epsilon = 0.01)
+{
+    return fabs(a - b) <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
 TEST_CASE("testing example")
 {
+
+
+    /* Init test */
     Matrix a(2, 2, {{1, 0}, {0, 1}});
 
     CHECK(a.getValue(0, 0) == 1);
@@ -11,10 +24,142 @@ TEST_CASE("testing example")
     CHECK(a.getValue(1, 1) == 1);
 
     Matrix b(2, 2, {{0, 1}, {1, 0}});
-    Matrix c = Matrix::addMat(a, b);
+    Matrix b2(2, 2, {{-3, -2}, {-2, 6}});
+    Matrix b3(2, 2, {{1.25, -5.25}, {-1.33, 0.33}});
 
+    Matrix vec3(3, 1, {{3}, {5}, {0.2}});
+    Matrix vec31(3, 1, {{0}, {1}, {0}});
+
+    /* Addition tests */
+    Matrix c = Matrix::addMat(a, b);
+    Matrix c2 = Matrix::addMat(a, b2);
+    Matrix c3 = Matrix::addMat(a, b3);
+
+    // Positive value
     CHECK(c.getValue(0, 0) == 1);
     CHECK(c.getValue(0, 1) == 1);
     CHECK(c.getValue(1, 0) == 1);
     CHECK(c.getValue(1, 1) == 1);
+
+    // Negative value
+    CHECK(c2.getValue(0, 0) == -2);
+    CHECK(c2.getValue(0, 1) == -2);
+    CHECK(c2.getValue(1, 0) == -2);
+    CHECK(c2.getValue(1, 1) == 7);
+
+    // float
+    CHECK(c3.getValue(0, 0) == 2.25);
+    CHECK(c3.getValue(0, 1) == -5.25);
+    CHECK(c3.getValue(1, 0) == -1.33);
+    CHECK(c3.getValue(1, 1) == 1.33);
+
+    /* Subtraction tests */
+    Matrix d = Matrix::subMat(a, b);
+    Matrix d2 = Matrix::subMat(a, b2);
+    Matrix d3 = Matrix::subMat(a, b3);
+
+    CHECK(d.getValue(0, 0) == 1);
+    CHECK(d.getValue(0, 1) == -1);
+    CHECK(d.getValue(1, 0) == -1);
+    CHECK(d.getValue(1, 1) == 1);
+
+    CHECK(d2.getValue(0, 0) == 4);
+    CHECK(d2.getValue(0, 1) == 2);
+    CHECK(d2.getValue(1, 0) == 2);
+    CHECK(d2.getValue(1, 1) == -5);
+
+    CHECK(absoluteToleranceCompare(d3.getValue(0, 0), -0.25));
+    CHECK(absoluteToleranceCompare(d3.getValue(0, 1), 5.25));
+    CHECK(absoluteToleranceCompare(d3.getValue(1, 0), 1.33));
+    CHECK(absoluteToleranceCompare(d3.getValue(1, 1), 0.67));
+
+    /* Multiplication with scalar */
+    Matrix e = Matrix::scalMult(a, 7);
+    Matrix e2 = Matrix::scalMult(a, -7);
+    Matrix e3 = Matrix::scalMult(a, 0.33);
+
+    CHECK(e.getValue(0, 0) == 7);
+    CHECK(e.getValue(0, 1) == 0);
+    CHECK(e.getValue(1, 0) == 0);
+    CHECK(e.getValue(1, 1) == 7);
+
+    CHECK(e2.getValue(0, 0) == -7);
+    CHECK(e2.getValue(0, 1) == 0);
+    CHECK(e2.getValue(1, 0) == 0);
+    CHECK(e2.getValue(1, 1) == -7);
+
+    CHECK(absoluteToleranceCompare(e3.getValue(0, 0), 0.33));
+    CHECK(absoluteToleranceCompare(e3.getValue(0, 1), 0));
+    CHECK(absoluteToleranceCompare(e3.getValue(1, 0), 0));
+    CHECK(absoluteToleranceCompare(e3.getValue(1, 1), 0.33));
+
+    /* Multiplication with another matrix */
+    Matrix f = Matrix::prodMat(a, a);
+    Matrix f2 = Matrix::prodMat(a, b);
+    Matrix f3 = Matrix::prodMat(b2, b3);
+
+    CHECK(f.getValue(0, 0) == 1);
+    CHECK(f.getValue(0, 1) == 0);
+    CHECK(f.getValue(1, 0) == 0);
+    CHECK(f.getValue(1, 1) == 1);
+
+    CHECK(f2.getValue(0, 0) == 0);
+    CHECK(f2.getValue(0, 1) == 1);
+    CHECK(f2.getValue(1, 0) == 1);
+    CHECK(f2.getValue(1, 1) == 0);
+
+    CHECK(absoluteToleranceCompare(f3.getValue(0, 0), -1.09));
+    CHECK(absoluteToleranceCompare(f3.getValue(0, 1), 15.09));
+    CHECK(absoluteToleranceCompare(f3.getValue(1, 0), -10.48));
+    CHECK(absoluteToleranceCompare(f3.getValue(1, 1), 12.48));
+
+    /* Rotation X, Y and Z */
+    Matrix g = Matrix::rotationX(0.25, vec3);
+    Matrix g2 = Matrix::rotationY(0.25, vec3);
+    Matrix g3 = Matrix::rotationZ(0.25, vec3);
+
+    CHECK(absoluteToleranceCompare(g.getValue(0, 0), 3));
+    CHECK(approximatelyEqual(g.getValue(1, 0), 4.79508));
+    CHECK(approximatelyEqual(g.getValue(2, 0), 1.4308022806));
+
+    CHECK(approximatelyEqual(g2.getValue(0, 0), 2.95622));
+    CHECK(approximatelyEqual(g2.getValue(1, 0), 5));
+    CHECK(approximatelyEqual(g2.getValue(2, 0), -0.548429));
+
+    CHECK(approximatelyEqual(g3.getValue(0, 0), 1.66972));
+    CHECK(approximatelyEqual(g3.getValue(1, 0), 5.58677));
+    CHECK(approximatelyEqual(g3.getValue(2, 0), 0.2));
+
+    /* Matrix transposition */
+    Matrix h = Matrix::transposed(a);
+    Matrix h1 = Matrix::transposed(vec3);
+    Matrix h2 = Matrix::transposed(b3);
+
+    CHECK(h.getValue(0, 0) == 1);
+    CHECK(h.getValue(0, 1) == 0);
+    CHECK(h.getValue(1, 0) == 0);
+    CHECK(h.getValue(1, 1) == 1);
+
+    CHECK(h1.getValue(0, 0) == 3);
+    CHECK(h1.getValue(0, 1) == 5);
+    CHECK(h1.getValue(0, 2) == 0.2);
+
+    CHECK(h2.getValue(0, 0) == 1.25);
+    CHECK(h2.getValue(0, 1) == -1.33);
+    CHECK(h2.getValue(1, 0) == -5.25);
+    CHECK(h2.getValue(1, 1) == 0.33);
+
+    /* Norm Calculation  */
+    double i = Matrix::getNorm(vec31);
+    double i2 = Matrix::getNorm(vec3);
+
+    CHECK(i == 1);
+    CHECK(approximatelyEqual(i2, 5.8343808583));
+
+    /* Normalize */
+    Matrix j = Matrix::normalize(vec31);
+    Matrix j2 = Matrix::normalize(vec3);
+
+    CHECK(Matrix::getNorm(j) == 1);
+    CHECK(approximatelyEqual(Matrix::getNorm(j2), 1.0));
 }
